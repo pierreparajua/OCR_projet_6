71049a7a930fa2548df5299caf6c4365e5ab89e8
pierreparajua <64963222+pierreparajua@users.noreply.github.com>
2022-02-17 18:40:24 +0100

    carroussel in progress
    Refactor function getData and Feat function createCarroussel

--- bestMovie.js
+++ bestMovie.js
@@ -1,115 +1,165 @@
 
-const urlBestMovie = 'http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=';
+const nbrMoviesToDisplay = 7
 
-const NbrPageByCategorie = 5
+const urlTitle = 'http://localhost:8000/api/v1/titles'
+const urlGenre = 'http://localhost:8000/api/v1/genres'
+const urlBestRanking = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=" + (nbrMoviesToDisplay + 1)
+const urlAction = "http://localhost:8000/api/v1/titles/?genre=action&sort_by=-imdb_score&page_size=" + nbrMoviesToDisplay 
+const urlComedy = "http://localhost:8000/api/v1/titles/?genre=comedy&sort_by=-imdb_score&page_size=7" + nbrMoviesToDisplay
+const urlAnimation = "http://localhost:8000/api/v1/titles/?genre=animation&sort_by=-imdb_score&page_size=7" + nbrMoviesToDisplay
 
 
-async function getBestMovie() {
-    const requete = await fetch(urlBestMovie, {
-        method: 'GET'
-    });
+
+
+
+async function getData(){
+    const requete = await fetch(urlBestRanking, {method: 'GET'});
     if (!requete.ok) {alert("Erreur de connexion")}
-    else { let data = await requete.json();
-        data = data.results[0]
-
-        document.querySelector('#title').textContent = data.title;
-        document.querySelector('#resume').textContent = "Super film";
-        document.querySelector("#containerBestMovie").style.backgroundImage="url(" + data.image_url + ")";
+    else { 
+        let data =   await requete.json();
+        data = data.results;
+        console.log(data);
+        getBestMovie(data[0]);
+        createCaroussel(data, "Meilleur films", 4)
     }
 }
+getData();
 
+function createDiv(id) {
+    let div = document.createElement('div');
+    div.setAttribute('id', id);
+    return div
+}
 
+function getBestMovie(movie) {
+    let div = createDiv("imgBestMovie")
+    div.style.backgroundImage ="url(" + movie.image_url + ")";
+    document.querySelector("#containerBestMovie").append(div);
+}
 
+function createCaroussel(movies, category, visible) {
+    let containerCarrousel = createDiv('containerCarrousel')
+    document.querySelector("#allCarrousels").append(containerCarrousel)
 
-async function getBestMovies(url) {
-    const urls = new Array(url)
-    const requete = await fetch(url, {method: 'GET'});
-    if (!requete.ok) {alert("Erreur de connexion")}
-    else { let data = await requete.json();
-        let next = data.next
-        urls.push(next)
-        for (let i = 3; i < NbrPageByCategorie+1; i++ ){
-            next = next.replace(i - 1, i)
-            urls.push(next)    
-        }
-    }    
-    const datasUrls = new Array()
-    const datasTitle = new Array()
-    urls.forEach(async function (url) {
-        const requete = await fetch(url, {method: 'GET'});
-        if (!requete.ok) {alert("Erreur de connexion")}
-        else { let data = await requete.json();
-            data = data.results
-            for (let i = 0; i < 5; i++ ){
-                datasUrls.push(data[i].image_url)
-                datasTitle.push(data[i].title)      
-         }
-        }
-    });
-    let datas = [[datasUrls], [datasTitle]]
-    return datas
+    let titleCategorie = document.createElement('h1')
+    titleCategorie.setAttribute('id', "tilteCategorie")
+    titleCategorie.textContent = category
+    document.querySelector("#containerCarrousel").append(titleCategorie)
+
+    let carrousel = createDiv("carrousel")
+    document.querySelector("#containerCarrousel").append(carrousel)
+
+    movies.slice(1, 8).forEach(movie => {
+        let imgMovie = createDiv("divMovie")
+        imgMovie.style.backgroundImage ="url(" + movie.image_url + ")";
+        document.querySelector("#carrousel").append(imgMovie)
+    })
+
+    let leftButton = document.createElement('button')
+    leftButton.setAttribute('id', "goLeft")
+    document.querySelector("#containerCarrousel").append(leftButton)
+
+    let rightButton = document.createElement('button')
+    rightButton.setAttribute('id', "goRight")
+    document.querySelector("#containerCarrousel").append(rightButton)
+    
+    leftButton.onclick = () => {
+        document.querySelector("#carrousel").style.transform = "translate3D(0, 0, 0)"}
+    rightButton.onclick = () => {
+        document.querySelector("#carrousel").style.transform = "translate3D(-24%, 0, 0)"}
 }
 
 
-let containerCarrousel = document.createElement('div')
-containerCarrousel.setAttribute('id', "containerCarrousel")
-document.querySelector("#allCarrousels").append(containerCarrousel)
 
-let titleCategorie = document.createElement('h1')
-titleCategorie.setAttribute('id', "tilteCategorie")
-titleCategorie.textContent = "Meilleur films"
-document.querySelector("#containerCarrousel").append(titleCategorie)
+// async function getBestMovies(url) {
+//     const urls = new Array(url)
+//     const requete = await fetch(url, {method: 'GET'});
+//     if (!requete.ok) {alert("Erreur de connexion")}
+//     else { let data = await requete.json();
+//         let next = data.next
+//         urls.push(next)
+//         for (let i = 3; i < NbrPageByCategorie+1; i++ ){
+//             next = next.replace(i - 1, i)
+//             urls.push(next)    
+//         }
+//     }    
+//     const datasUrls = new Array()
+//     const datasTitle = new Array()
+//     urls.forEach(async function (url) {
+//         const requete = await fetch(url, {method: 'GET'});
+//         if (!requete.ok) {alert("Erreur de connexion")}
+//         else { let data = await requete.json();
+//             data = data.results
+//             for (let i = 0; i < 5; i++ ){
+//                 datasUrls.push(data[i].image_url)
+//                 datasTitle.push(data[i].title)      
+//          }
+//         }
+//     });
+//     let datas = [[datasUrls], [datasTitle]]
+//     return datas
+// }
 
-let carrousel = document.createElement('div')
-carrousel.setAttribute('id', "carrousel")
-document.querySelector("#containerCarrousel").append(carrousel)
 
-getBestMovie()
-const datas = getBestMovies(urlBestMovie)
-console.log(datas[0])
-for (let i = 0; i < (NbrPageByCategorie*5); i++){
-    //console.log(datas[0][i])
-    let divMovie = document.createElement('div')
-    divMovie.setAttribute('class', "divMovie")
-    //divMovie.style.backgroundImage ="url('" + datas[0][i] +"')";
-    document.querySelector("#carrousel").append(divMovie);
-} 
+// let containerCarrousel = document.createElement('div')
+// containerCarrousel.setAttribute('id', "containerCarrousel")
+// document.querySelector("#allCarrousels").append(containerCarrousel)
+
+// let titleCategorie = document.createElement('h1')
+// titleCategorie.setAttribute('id', "tilteCategorie")
+// titleCategorie.textContent = "Meilleur films"
+// document.querySelector("#containerCarrousel").append(titleCategorie)
+
+// let carrousel = document.createElement('div')
+// carrousel.setAttribute('id', "carrousel")
+// document.querySelector("#containerCarrousel").append(carrousel)
+
+// getBestMovie()
+// const datas = getBestMovies(urlBestMovie)
+// console.log(datas)
+// for (let i = 0; i < (NbrPageByCategorie*5); i++){
+//     console.log(datas[0][i])
+//     //let divMovie = document.createElement('div')
+//     divMovie.setAttribute('class', "divMovie")
+//     //divMovie.style.backgroundImage ="url('" + datas[0][i] +"')";
+//     document.querySelector("#carrousel").append(divMovie);
+// } 
 
 
-    // let divMovie = document.createElement('div')
-    // divMovie.setAttribute('class', "divMovie")
-    // divMovie.style.backgroundImage = ""+ datas[index].image_url +"";
-    // document.querySelector("#carrousel").append(divMovie);
+//     // let divMovie = document.createElement('div')
+//     // divMovie.setAttribute('class', "divMovie")
+//     // divMovie.style.backgroundImage = ""+ datas[index].image_url +"";
+//     // document.querySelector("#carrousel").append(divMovie);
     
 
 
 
 
-function goRight() {
-    let translateX = -100 / 7
-    document.querySelector("#carrousel").style.transform = "translate3D(-13.8%, 0, 0)"
-}
-function goLeft() {
-    let translateX = -100 / 7
-    document.querySelector("#carrousel").style.transform = "translate3D(0, 0, 0)"
-}
+// function goRight() {
+//     let translateX = -100 / 7
+//     document.querySelector("#carrousel").style.transform = "translate3D(-13.8%, 0, 0)"
+// }
+// function goLeft() {
+//     let translateX = -100 / 7
+//     document.querySelector("#carrousel").style.transform = "translate3D(0, 0, 0)"
+// }
 
-let leftButton = document.createElement('button')
-leftButton.setAttribute('id', "goLeft")
-document.querySelector("#containerCarrousel").append(leftButton)
+// let leftButton = document.createElement('button')
+// leftButton.setAttribute('id', "goLeft")
+// document.querySelector("#containerCarrousel").append(leftButton)
 
-let rightButton = document.createElement('button')
-rightButton.setAttribute('id', "goRight")
-document.querySelector("#containerCarrousel").append(rightButton)
+// let rightButton = document.createElement('button')
+// rightButton.setAttribute('id', "goRight")
+// document.querySelector("#containerCarrousel").append(rightButton)
 
 
 
-leftButton.onclick = () => {
-    document.querySelector("#carrousel").style.transform = "translate3D(0, 0, 0)"
-}
-rightButton.onclick = () => {
-    document.querySelector("#carrousel").style.transform = "translate3D(-13.8%, 0, 0)"
-}
+// leftButton.onclick = () => {
+//     document.querySelector("#carrousel").style.transform = "translate3D(0, 0, 0)"
+// }
+// rightButton.onclick = () => {
+//     document.querySelector("#carrousel").style.transform = "translate3D(-13.8%, 0, 0)"
+// }
 
 // rightButton.addEventListener('click', goRight)
 // leftButton.addEventListener('click', goLeft)
@@ -118,12 +168,12 @@ rightButton.onclick = () => {
 // divMovie.setAttribute('class', "divMovie")
 // divMovie.style.backgroundImage = ""+ data[i].image_url +"";
 // document.querySelector("#carrousel").append(divMovie)
-                //document.querySelector(".divMovie").className = "divMovie";
-                // let img = document.createElement('img')
-                // img.src=data[i].image_url;
-                // img.title=data[i].title;
-                // document.querySelector('.divMovie').append(img)
-                // img.id="movie"
+//                 document.querySelector(".divMovie").className = "divMovie";
+//                 let img = document.createElement('img')
+//                 img.src=data[i].image_url;
+//                 img.title=data[i].title;
+//                 document.querySelector('.divMovie').append(img)
+//                 img.id="movie"
 
 
 
